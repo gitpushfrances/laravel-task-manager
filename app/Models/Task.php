ae083f4bc17222eacdd50053e0db7545ee5f03d2
John Frances Eder <johnfrancesquiminaleseder@gmail.com>
2025-09-12 00:36:57 +0800

    Initial commit - Laravel project setup

---
+++ app/Models/Task.php
@@ -0,0 +1,12 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+    ];
+}
